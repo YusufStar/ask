89839829ab1d3987835cef6b278ee563e0ed281b
Yusuf yıldız <me@yusufstar.com>
2025-05-26 00:30:49 +0300

    fix: correct remotePatterns configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,11 @@ const nextConfig: NextConfig = {
         protocol: "http",
         hostname: "localhost",
         port: "8000",
-      }
+      },
+      {
+        protocol: "https",
+        hostname: "ad83-2a09-bac5-58c2-4d2-00-7b-61.ngrok-free.app",
+      },
     ]
   }
 };
